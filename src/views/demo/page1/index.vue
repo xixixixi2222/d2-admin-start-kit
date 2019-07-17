d75a5315a72928d7066745a0fd0b73e56d9e5e7e
xixixixi2222 <38251946+xixixixi2222@users.noreply.github.com>
2019-07-17 12:10:47 +0800

    Update index.vue

--- src/views/demo/page1/index.vue
+++ src/views/demo/page1/index.vue
@@ -1,12 +1,32 @@
-<template>
-  <d2-container>
-    <template slot="header">Page 1 header</template>
-    Hello World
-  </d2-container>
-</template>
+<el-button type="text" @click="dialogVisible = true">点击打开 Dialog</el-button>
+
+<el-dialog
+  title="提示"
+  :visible.sync="dialogVisible"
+  width="30%"
+  :before-close="handleClose">
+  <span>这是一段信息</span>
+  <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+  </span>
+</el-dialog>
 
 <script>
-export default {
-  name: 'page1'
-}
+  export default {
+    data() {
+      return {
+        dialogVisible: false
+      };
+    },
+    methods: {
+      handleClose(done) {
+        this.$confirm('确认关闭？')
+          .then(_ => {
+            done();
+          })
+          .catch(_ => {});
+      }
+    }
+  };
 </script>
